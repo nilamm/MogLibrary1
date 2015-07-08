f1bad33bde97e9ebb0a7be68b53cd46704538fab
fezziwig <nicholas.lamm@gmail.com>
2015-07-08 15:25:07 -0400

    on postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150706032225) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "checkouts", force: :cascade do |t|
     t.integer  "num_checked"
     t.string   "notes"
